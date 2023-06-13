23ad469fe90b2bf22978329d751f778f42b8c843
Th1s1s1t <110562298+Th1s1s1t@users.noreply.github.com>
2023-06-13 11:23:03 +0200

    Clean up ApiService object

--- app/src/main/java/com/example/myapplication/ApiService.kt
+++ app/src/main/java/com/example/myapplication/ApiService.kt
@@ -8,7 +8,9 @@ import okhttp3.logging.HttpLoggingInterceptor
 import java.io.IOException
 
 object ApiService {
-    private const val BASE_URL = "http://192.168.1.55:7861/sdapi/v1/txt2img"
+
+    // Declare Base URL to connect to
+    private const val BASE_URL = "http://192.168.1.55:7861/sdapi/v1/"
 
     private val client: OkHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
@@ -16,7 +18,8 @@ object ApiService {
         })
         .build()
 
-    fun sendPostRequest(prompt: String, steps: Int, width: Int, height: Int): ByteArray? {
+    // Function for Image Generation
+    fun sendPostRequestImg(prompt: String, steps: Int, width: Int, height: Int): ByteArray? {
         val mediaType = "application/json".toMediaType()
         val requestBody = """
             {
@@ -28,7 +31,7 @@ object ApiService {
         """.trimIndent().toRequestBody(mediaType)
 
         val request = Request.Builder()
-            .url(BASE_URL)
+            .url(BASE_URL + "txt2img")
             .post(requestBody)
             .build()
 
