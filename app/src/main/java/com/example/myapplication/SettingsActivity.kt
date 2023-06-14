230a557eddf0810c2a46b6758a549f8ffab2bfc4
Th1s1s1t <110562298+Th1s1s1t@users.noreply.github.com>
2023-06-14 11:29:14 +0200

    Adding Reload Option to Settings && Adding a Get All Available Models Option

--- app/src/main/java/com/example/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -2,15 +2,34 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var reload: Button
+    private lateinit var save: Button
+    private lateinit var modelNamesSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings)
+
+        reload = findViewById(R.id.reload)
+        save = findViewById(R.id.save)
+        modelNamesSpinner = findViewById(R.id.modelNamesSpinner)
 
         val switchButton2 = findViewById<Button>(R.id.switchButton2)
         switchButton2.setOnClickListener {
@@ -20,8 +39,45 @@ class SettingsActivity : AppCompatActivity() {
             finish() // Finish the current activity
         }
 
-        // Rest of your code...
+        // Get Info from backend
+        reload.setOnClickListener {
+            getModels()
+        }
+
+        // Forward new options
+        save.setOnClickListener {
+            // Post Request to change settings
+        }
     }
 
-    // Rest of your code...
+    private fun getModels() {
+        GlobalScope.launch {
+            val response = ApiService.sendGetRequest()
+            withContext(Dispatchers.Main) {
+                if (response != null) {
+                    try {
+                        val jsonArray = JSONArray(response)
+                        val modelNames = ArrayList<String>()
+
+                        for (i in 0 until jsonArray.length()) {
+                            val jsonObject: JSONObject = jsonArray.getJSONObject(i)
+                            val modelName: String = jsonObject.getString("model_name")
+                            modelNames.add(modelName)
+                        }
+
+                        val adapter = ArrayAdapter(
+                            this@SettingsActivity,
+                            android.R.layout.simple_spinner_item,
+                            modelNames
+                        )
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        modelNamesSpinner.adapter = adapter
+
+                    } catch (e: JSONException) {
+                        Log.e("JSONParsing", "Error parsing JSON response: ${e.message}")
+                    }
+                }
+            }
+        }
+    }
 }
